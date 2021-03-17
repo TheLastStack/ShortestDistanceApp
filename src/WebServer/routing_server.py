0d424a084d4397fcc5dbd538897d9fb83837e93e
N Mahen <f20170376@hyderabad.bits-pilani.ac.in>
2021-03-17 13:41:08 +0530

    Wrote part of the build script. Makes and loads data into database

--- src/WebServer/routing_server.py
+++ src/WebServer/routing_server.py
@@ -3,15 +3,27 @@ from flask import render_template
 from flask import request
 import dicttoxml
 import random
+
 app = Flask(__name__)
+
+DB_USER = 'postgres' # Default user is postgres
+DB_PASSOWRD = '' # Need password to access database
+DB_NAME = 'osm'
 
 @app.route('/')
 def start():
     return render_template('route_map.html')
 
+def update_map(min_x, min_y, max_x, max_y, file_id):
+
+
 @app.route('/navigate', methods=['POST'])
 def gotcoords():
     print(request.form) #Received POST data in request.form
+    entered_points = {}
+    for key, value in request.form:
+        entered_points[key] = value
+    # Received Points are present in dictionary here.
     # A* algorithm here
     # Path from A* will be of form: [(x0, y0), (x1, y1), ...]
     # Below statement is a placeholder
---
+++ src/server_build.py
@@ -0,0 +1,76 @@
+from urllib import parse
+from urllib import request as req
+import os
+import sys
+import subprocess
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='This script initializes the postGIS database.'
+        'Ensure you have installed PostgreSQL and PostGIS extension before starting the installation')
+    parser.add_argument('-U','--DBUSER', default='postgres', help="User Name of PostGIS database")
+    parser.add_argument('-d', '--DBNAME', default='osm', help="Name of PostGIS database to create/overwrite")
+    parser.add_argument('-W', '--DBPASSWORD', help="Password for user authentication")
+    parser.add_argument('-H', '--DBHOST', default='localhost', help="Network address of database server")
+    parser.add_argument('-P', '--DBPORT', default="5432", help="Port for the server")
+    parser.add_argument('-X', '--XML', help="OSM XML file to use")
+    parser.add_argument('-L', nargs=4, help="Co-ordinates to restrict query to. Write in format Xmin Ymin Xmax Ymax")
+    args = parser.parse_args();
+    print(args)
+    DB_USER = args.DBUSER
+    DB_NAME = args.DBNAME
+    DB_PASSWORD = args.DBPASSWORD
+    DB_HOST = args.DBHOST
+    DB_PORT = args.DBPORT
+    XML_NAME = args.XML
+    PREFIX_STRING = "postgresql://{}:{}@{}:{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
+    if input("Do you want to create a database (Y/N)?").lower() == "y":
+        if XML_NAME is None:
+            if args.L is not None:
+                COORDS = [float(i) for i in args.L]
+            else:
+                sys.exit(1)
+            custom_min = lambda x : (round(min(x) * 50) - 1) / 50
+            custom_max = lambda x : (round(max(x) * 50) + 1) / 50
+            min_x = custom_min((COORDS[0],))
+            min_y = custom_min((COORDS[1],))
+            max_x = custom_max((COORDS[2],))
+            max_y = custom_max((COORDS[3],))
+            request_string = "(way[\"highway\"]({},{},{},{});node(w););out body;"
+            req.urlretrieve('https://overpass-api.de/api/interpreter?data=' +
+                                parse.quote_plus(request_string.format(min_x, min_y, max_x,max_y),
+                                                safe="[]();.,"),
+                             os.path.join(os.getcwd(), "osm_d.osm"))
+            XML_NAME = 'osm_d.osm'
+        print(PREFIX_STRING)
+        if os.name.lower() in ['windows', 'nt']:
+            if subprocess.call("powershell -Command \"$env:PGPASSWORD='{}'; psql -U {} -c 'create database {};'\"".format(DB_PASSWORD, DB_USER, DB_NAME)) != 0:
+                sys.exit(1)
+            if subprocess.call("powershell -Command \"$env:PGPASSWORD='{}'; psql -U {} -d {} -c 'create extension postgis;'\"".format(DB_PASSWORD, DB_USER, DB_NAME)) != 0:
+                sys.exit(1)
+            if subprocess.call("powershell -Command \"$env:PGPASSWORD='{}'; psql -U {} -d {} -c 'create extension hstore;'\"".format(DB_PASSWORD, DB_USER, DB_NAME)) != 0:
+                sys.exit(1)
+        else:
+            if subprocess.run(['psql', PREFIX_STRING, 'create database', '-e', 'utf8', '{}'.format(DB_NAME)], shell=True, capture_output=True).stderr is not None:
+                sys.exit(1)
+            if subprocess.run(['psql', PREFIX_STRING, 'create extension postgis']).stderr is not None:
+                sys.exit(1)
+            if subprocess.run(['psql', PREFIX_STRING, 'create extension hstore']).stderr is not None:
+                sys.exit(1)
+        if os.name.lower() in ['windows', 'nt']:
+            try:
+                path = input('Enter path to osm2pgsql binary>')
+                output = subprocess.run(
+                    [path + '\\osm2pgsql',
+                     '-S', '{}\\default.style'.format(path), '-W', '-U', DB_USER, '-d', DB_NAME, '-r', 'xml', '-H', DB_HOST, '-P', DB_PORT, XML_NAME],
+                    shell=True, check=True)
+            except subprocess.CalledProcessError as exc:
+                print("Status : FAIL", exc.returncode, exc.output)
+                sys.exit(1)
+        else:
+            output = subprocess.run(
+                ['osm2pgsql',
+                 '-s', '-W', '-U', DB_USER, '-d', DB_NAME, '-r', 'xml', '-H', DB_HOST, '-P', DB_PORT, XML_NAME],
+                shell=True, check=True)
+        with open("credentials.key", "w") as location:
+            location.write(PREFIX_STRING)
